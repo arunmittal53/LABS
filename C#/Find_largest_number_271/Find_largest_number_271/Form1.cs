55cec63c547da38b2459c4e1ecc979977ec69e4e
Arun Mittal <arunmittal53@users.noreply.github.com>
2017-01-05 14:25:42 +0530

    Add files via upload

---
+++ C#/Find_largest_number_271/Find_largest_number_271/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Find_largest_number_271
+{
+    public partial class Form1 : Form
+    {
+        int num1, mini1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = int.Parse(textBox1.Text);
+                if (num1 == -1)
+                    textBox1.Text = mini1.ToString();
+                else
+                {
+                    if (mini1 < num1)
+                        mini1 = num1;
+                    textBox1.Text = null;
+                }
+            }
+            catch (Exception err)
+            {
+                textBox1.Text = err.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            mini1 = 0;
+        }
+    }
+}
---
+++ C#/Find_largest_number_271/Find_largest_number_271/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Find_largest_number_271
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            str = textBox1.Text;
+            int i, n;
+            i = 0; n = str.Length - 1;
+            while (i < n)
+            {
+                if (str[i] != str[n])
+                {
+                    textBox1.Text = "NOT PALINDROME";
+                    break;
+                }
+                i++; n--;
+            }
+            if (i >= n)
+            {
+                textBox1.Text = "PALINDROME";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+        }
+    }
+}
